3904db1ce96f35ab2088b90637fb43808f0e4aac
Tarun Krishnan <tarun@colossal.net>
2016-08-30 19:40:28 -0400

    showed existing journal entries

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,7 +9,26 @@
                 <div class="form-content hidden"></div>
             </div>
         </div>
+        <div class="clearfix"></div>
+        <br><br>
+        <h5>Past Journal Entries</h5>
+        <div class="list-group">
+            @foreach(App\Journal::orderby('created_at', 'desc')->get() as $journal)
+                <div class="list-group-item">
+                    <h5 class="list-group-item-heading">{{ date('F jS, Y \a\t g:ia', strtotime($journal->created_at)) }}</h5>
+                    <div class="list-group-item-text">
+                        <small>
+                            {!! \App\Situation::where('journal_id', $journal->id)->first() ? '<span class="tag tag-warning">sitch</span> '.substr(\App\Situation::where('journal_id', $journal->id)->first()->content, 0, 35).'&hellip;' : null !!}
+                            {!! \App\Action::where('journal_id', $journal->id)->first() ? '<br/><span class="tag tag-success">sitch</span> '.substr(\App\Action::where('journal_id', $journal->id)->first()->content, 0, 35).'&hellip;' : null !!}
+                        </small>
+                        <br><br>
+                        <a href="{{ URL::to($journal->id.'/journal') }}" class="btn btn-secondary btn-sm">View</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
+
 
 @stop
 
